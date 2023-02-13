76dde8a9ccddaa469d2ecfe42fa4d5fe4cab266d
Joshua V Arnaez <jarnaez@yondu.com>
2023-02-13 23:48:50 +0800

    ..

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,6 +1,5 @@
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import { getServerSession } from "next-auth/next";
 import prisma from "@/lib/prisma";
 import bcrypt from "bcrypt";
 
