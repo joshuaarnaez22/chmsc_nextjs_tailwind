bc17b21f410834be139a24302081ec928e8d7579
Joshua V Arnaez <jarnaez@yondu.com>
2023-02-14 06:17:30 +0800

    ..

--- pages/index.js
+++ pages/index.js
@@ -10,8 +10,7 @@ import { getServerSession } from "next-auth/next";
 import React from "react";
 import { useRouter } from "next/router";
 
-function Home() {
-  // { user }
+function Home({ user }) {
   const router = useRouter();
   const [email, setEmail] = react.useState("");
   const [password, setPassword] = react.useState("");
@@ -26,13 +25,13 @@ function Home() {
     if (result.status === 200) router.replace(router.asPath);
   };
 
-  // if (user) {
-  //   return (
-  //     <>
-  //       <button onClick={signOut}>Logout</button>
-  //     </>
-  //   );
-  // }
+  if (user) {
+    return (
+      <>
+        <button onClick={signOut}>Logout</button>
+      </>
+    );
+  }
 
   return (
     <>
@@ -166,14 +165,14 @@ function Home() {
   );
 }
 
-// export async function getServerSideProps({ req, res }) {
-//   const session = await getServerSession(req, res, authOptions);
-//   const user = session?.user || null;
-//   return {
-//     props: {
-//       user,
-//     },
-//   };
-// }
+export async function getServerSideProps({ req, res }) {
+  const session = await getServerSession(req, res, authOptions);
+  const user = session?.user || null;
+  return {
+    props: {
+      user,
+    },
+  };
+}
 
 export default Home;
